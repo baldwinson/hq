0149f74917c81ca648e04e2c99107388fc8f0053
Curtis Baldwinson <curtis@baldwinson.com>
2025-12-18 22:28:01 -0800

    Ethics email waitlist implementation

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
@@ -8,6 +9,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baldwinson.com',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react(), sitemap()],
 
   vite: {
